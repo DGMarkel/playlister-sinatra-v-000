3771e738881e949ce5f2f71786ac1d26f670d497
David Markel <davidgmarkel@gmail.com>
2018-06-13 15:52:20 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,13 @@
-require_relative './concerns/slugifiable'
-
 class Artist < ActiveRecord::Base
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
   has_many :songs
   has_many :genres, through: :songs
 
+  def slug
+    self.name.gsub(" ", "-").downcase
+  end
 
+  def self.find_by_slug(slug)
+    self.all.find {|artist| artist.slug == slug}
+  end
 
 end
